879a7c666786fcfa1c3f7eda78e9cff4b1b45e4e
이상철 <zlrz6706@gmail.com>
2022-08-04 09:14:38 +0900

    news List component

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [data, setData] = useState(null)
+  const onClick = async() => {
+      try {
+      const respons =  await axios.get(
+            'https://newsapi.org/v2/top-headlines?country=kr&apiKey=99b9486811fe445f8cc48237ee99a883'
+          );
+          setData(respons)
+      }catch(e) {
+        console.log(e)
+      }
+    }
   return (
     <div>
-      hello world
+      <div>
+        <button onClick={onClick}>불러기기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
     </div>
   );
 };
